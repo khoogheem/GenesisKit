855b02862188ad5e7f3ffe361c91aa0930b01ef4
Kevin A. Hoogheem <kevin@hoogheem.net>
2014-10-25 18:14:06 -0500

    headerupdates

--- GenesisKitOSX/GenesisKitOSX.h
+++ GenesisKitOSX/GenesisKitOSX.h
@@ -1,6 +1,6 @@
 //
 //  GenesisKitOSX.h
-//  GenesisKitOSX
+//  GenesisKit
 //
 //  Created by Kevin A. Hoogheem on 10/25/14.
 //  Copyright (c) 2014 Kevin A. Hoogheem. All rights reserved.
--- GenesisKitOSXTests/GenesisKitOSXTests.swift
+++ GenesisKitOSXTests/GenesisKitOSXTests.swift
@@ -1,6 +1,6 @@
 //
 //  GenesisKitOSXTests.swift
-//  GenesisKitOSXTests
+//  GenesisKit
 //
 //  Created by Kevin A. Hoogheem on 10/25/14.
 //  Copyright (c) 2014 Kevin A. Hoogheem. All rights reserved.
