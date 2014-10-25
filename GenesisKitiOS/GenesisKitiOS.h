e5b5f0e2fd5bb4c6352d5e338a9efbf97b9eaebc
Kevin A. Hoogheem <kevin@hoogheem.net>
2014-10-25 18:08:27 -0500

    updates

--- GenesisKitiOS/GenesisKitiOS.h
+++ GenesisKitiOS/GenesisKitiOS.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double GenesisKitIOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char GenesisKitIOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GenesisKitIOS/PublicHeader.h>
-
-
